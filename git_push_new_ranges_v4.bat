efc3b60dd583258d7660c82ff71a04e76a7ef5f1
OLD_TS品檢蔡曜宇 <edwardtsai@igs.com.tw>
2026-06-04 20:42:45 +0800

    Remove disclaimer from intro screen

--- git_push_new_ranges_v4.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Adjust questions options scores and mapping to reduce value investing frequency"
-"C:\Program Files\Git\cmd\git.exe" push origin main
---
+++ git_push_remove_disclaimer.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Remove disclaimer from intro screen"
+"C:\Program Files\Git\cmd\git.exe" push origin main
