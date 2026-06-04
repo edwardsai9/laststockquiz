3f9b6fd74a6ae8a5465da87cbed358d2aa037b69
OLD_TS品檢蔡曜宇 <edwardtsai@igs.com.tw>
2026-06-04 20:43:29 +0800

    Left-align strategy analysis description text

--- git_push_left_align.bat
+++ git_push_left_align.bat
@@ -1,3 +1,3 @@
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Remove disclaimer from intro screen"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Left-align strategy analysis description text"
 "C:\Program Files\Git\cmd\git.exe" push origin main
